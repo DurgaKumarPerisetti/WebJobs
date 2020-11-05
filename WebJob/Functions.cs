2765fbde26dc24d97e77d9614908103836070014
unknown <dotnetcloudmc122@iiht.tech>
2020-11-05 15:54:15 +0530

    First Commit

---
+++ WebJob/Functions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebJobsSDKSample
+{
+    public class Functions
+    {
+        public static void ProcessQueueMessage([QueueTrigger("testq")] string message, ILogger logger)
+        {
+            logger.LogInformation(message);
+        }
+
+        public void MyTimerTriggerOperation([TimerTrigger("0 */5 * * * *", RunOnStartup = true)] TimerInfo timerInfo)
+        {
+            Console.WriteLine("Web job is completed now..!    " + DateTime.Now);
+            if (timerInfo.IsPastDue == true)
+            {
+                Console.WriteLine("The job is Delayed");
+            } else {
+                Console.WriteLine("The job is completed on time");
+            }
+        }
+     }
+}
